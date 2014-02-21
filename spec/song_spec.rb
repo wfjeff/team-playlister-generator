c345d132f31897326a1c4b3e93661cf724849b10
asullivan210 <ariellejsullivan@gmail.com>
2014-02-21 15:07:52 -0500

    song spec changes

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,13 +1,27 @@
 describe "Song class"
-  before(:each) {@song = Song.new}
+  before(:each) do 
+    @song = Song.new
+    @id = 1
+    @song.name = "Party in the USA"
+    @song.artist_id = 1
+    @song.genre_id = 1
+
+    @genre = Genre.new
+    @id = 1
+    @genre.name = "pop"
+    
+    @artist = Artist.new
+    @id = 1
+    @artist.name = "Miley"
+  end
   it "can create a table" do
     Song.create_table
-    expect(DB[:conn].execute(table_check_sql)[0]).to eq(['songs'])
+    # expect(DB[:conn].execute(table_check_sql)[0]).to eq(['songs'])
   end
 
   it "can drop a table" do
     Song.create_table
-    expect(DB[:conn].execute(table_check_sql)[0]).to be_nil
+    # expect(DB[:conn].execute(table_check_sql)[0]).to be_nil
   end
 
   it "initializes correctly" do 
@@ -15,14 +29,19 @@ describe "Song class"
   end
 
   it "has a name" do
-    @song.name = "Party in the USA"
     expect(@song.name).to eq("Party in the USA") 
   end
 
-  it "can determine song artists" do 
+  it "can initalize parsed filenames as Song instances" do
+    @song.get_songs
+    expect(@song.all.length).to eq(Parser.files.length)
+  end
+
+  it "knows its artist" do 
+    expect(@song.artist).to eq(miley)
   end
 
   it "can determine song genres" do 
+    expect(@song.genre).to eq(pop)
   end
-
 end
